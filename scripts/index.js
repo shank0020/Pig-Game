efc4ddc8589f8d19f02bae8a2bf1c8ea08ec6581
shank0020 <shashank.cse20@gmail.com>
2020-07-31 02:30:15 +0530

    add event listener to change the dice image whenever roll button is clicked

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,25 @@
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6 + 1);
-console.log(dice);
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+document.getElementById('score-0').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-1').textContent = 0;
 
 var x = document.querySelector('#score-' + activePlayer).textContent;
 document.querySelector('.dice').style.display = 'none'
+
+document.addEventListener('click', function() {
+    // 1. Random Number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = './images/dice-' + dice + '.png';
+});
